df097bde662fcb4b928e773886ed31af488fea70
Kirill Golovan <golovan.k@yandex.ru>
2019-06-05 22:46:33 +0300

    create basic mvvm structure

---
+++ TimetableGenerator/ViewModels/MainWindowVM.cs
@@ -0,0 +1,61 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using TimetableGenerator.Views;
+
+namespace TimetableGenerator.ViewModels
+{
+    class MainWindowVM : ViewModelBase
+    {
+
+        public DelegateCommand<Window> OpenDatabaseWindow
+        {
+            get
+            {
+
+                return new DelegateCommand<Window>((w) =>
+                {                    
+                    DatabaseConfigureWindow window = new DatabaseConfigureWindow();
+                    DatabaseConfigureWindowVM vm = new DatabaseConfigureWindowVM();
+                    window.DataContext = vm;
+                    w.Close();
+                    window.Show();
+                    //MainWindow mainWindow = new MainWindow();
+                    //MainWindowViewModel vm = new MainWindowViewModel();
+                    //mainWindow.DataContext = vm;
+                    //w.Close();
+                    //mainWindow.Show();
+
+
+                });
+
+            }
+        }
+        public DelegateCommand<Window> OpenConfigureTimetableWindow
+        {
+            get
+            {
+
+                return new DelegateCommand<Window>((w) =>
+                {
+                    ConfigureTimetable window = new ConfigureTimetable();
+                    ConfigureTimetableVM vm = new ConfigureTimetableVM();
+                    window.DataContext = vm;
+                    w.Close();
+                    window.Show();
+                    //MainWindow mainWindow = new MainWindow();
+                    //MainWindowViewModel vm = new MainWindowViewModel();
+                    //mainWindow.DataContext = vm;
+                    //w.Close();
+                    //mainWindow.Show();
+
+
+                });
+
+            }
+        }
+    }
+}
